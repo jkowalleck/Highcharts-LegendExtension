55373494e4354e2f7e05835202e2a63308619b6d
Jan Kowalleck <jan.kowalleck@googlemail.com>
2014-04-19 16:38:11 +0200

    - wrote README - moved LICENSE to own file - added parameter to legendToggle() as suggested in   https://bitbucket.org/jkowalleck/highcharts-legendextension/pull-request/1

--- Highcharts-LegendExtension.src.js
+++ Highcharts-LegendExtension.src.js
@@ -1,66 +1,45 @@
 // ==ClosureCompiler==
 // @compilation_level SIMPLE_OPTIMIZATIONS
 
-/*
 
- The MIT License (MIT)
-
- Copyright (c) 2013 Jan Kowalleck <jan.kowalleck@googlemail.com>
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
-
-*/
 
 /**
  * Extension for Highcharts3 and Highstocks1.3 to add the Chart some functions:
- * legendHide() , legendShow() , legendToggle() 
+ * legendHide() , legendShow() , legendToggle([forceState])
  */
-(function (Highcharts, UNDEFINED) {
+(function (Highcharts, UNDEFINED)
+{	"use strict";
 
-	if ( ! Highcharts ) 
+
+	if ( ! Highcharts )
 	{
 		return;
 	}
 
-	
+
 	var chartProto = Highcharts.Chart.prototype
 	  , legendProto = Highcharts.Legend.prototype
-	  ; 
+	  ;
 
 
-	Highcharts.extend(chartProto , 
+	Highcharts.extend(chartProto ,
 	{
-	
+
 		/**
 		 * set the visibility of the legend
-		 * 
-		 * @param {Boolean} display Whether to show or hide the the legend
+		 *
+		 * @param {Boolean} display Whether to show or hide the legend
 		 */
-		legendSetVisibility : function (display) 
+		legendSetVisibility : function (display)
 		{
+
 			var chart = this
 			  , legend = chart.legend , legendAllItems , legendAllItem , legendAllItemLength
 			  , legendOptions = chart.options.legend
 			  , scroller , extremes
 			  ;
 
-			if ( legendOptions.enabled == display ) 
+			if ( legendOptions.enabled == display )
 			{
 				return;
 			}
@@ -70,7 +49,7 @@
 			if ( ! display )
 			{
 				legendProto.destroy.call(legend);
-				
+
 				{  // fix for ex-rendered items - so they will be re-rendered if needed
 					legendAllItems = legend.allItems;
 					if ( legendAllItems )
@@ -81,7 +60,7 @@
 						}
 					}
 				}
-				
+
 				{ // fix for chart.endResize-eventListener and legend.positionCheckboxes()
 					legend.group = {};
 				}
@@ -98,31 +77,39 @@
 					scroller.render(extremes.min, extremes.max);
 				}
 			}
-		},
-	
+		} ,
+
 		/**
-		 * hide the legend 
+		 * hide the legend
 		 */
 		legendHide : function ()
 		{
 			this.legendSetVisibility(false);
-		},
+		} ,
 
 		/**
-		 * show the legend 
+		 * show the legend
 		 */
 		legendShow : function ()
 		{
 			this.legendSetVisibility(true);
-		},
+		} ,
 
 		/**
-		 * toggle the visibility of the legend 
+		 * toggle the visibility of the legend
+		 *
+		 * @param {Boolean} [display] optional: whether to show or hide the legend
 		 */
-		legendToggle : function ()
+		legendToggle : function (display)
 		{
-			this.legendSetVisibility(this.options.legend.enabled ^ true );
+			if ( typeof display != "boolean" )
+			{
+				display = ( this.options.legend.enabled ^ true );
+			}
+
+			this.legendSetVisibility(display);
 		}
-		
+
 	});
+
 }(Highcharts));
---
+++ README.md
@@ -0,0 +1,19 @@
+# HighchartsExtension: LegendToggle
+
+Extension for Highcharts3 and Highstocks1.3 
+to add the Chart some functions:
+
+* legendHide() 
+* legendShow() 
+* legendToggle() 
+
+
+
+## Demo
+* http://jsfiddle.net/jnkowa/63kMv/
+* http://jsfiddle.net/jnkowa/BnD8D/
+
+
+
+## License
+see file: LICENSE
